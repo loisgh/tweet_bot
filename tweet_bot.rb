dbb36c5f02cd142a35544cca415059eab5e15b52
loisgh <loisgh@nyc.rr.com>
2016-09-02 15:00:37 -0400

    make one minor improvement

--- tweet_bot.rb
+++ tweet_bot.rb
@@ -1,30 +1,26 @@
-@rand_char = Proc.new do
-  rand(36**1).to_s(36)
+def rand_char(len)
+  rand(36**len).to_s(36)
 end
 
 def check_password_strength(in_string)
-    num_char_types = count_character_types(in_string)
-    out_pw = check_dictionary(in_string)
-    out_length = out_pw.length
-    pw_strength = num_char_types * out_length
-    message = ''
-    if pw_strength >= 50
-      message = "Congratulations on your strong password"
-    elsif (11..49) === pw_strength
-      message =  out_pw << fix_password(num_char_types, out_length)
-    elsif pw_strength <= 10
-      message = "Your password is not adequate.  Please try again."
-    end
-    message
+  num_char_types = count_character_types(in_string)
+  out_pw = check_dictionary(in_string)
+  out_length = out_pw.length
+  pw_strength = num_char_types * out_length
+  message = ''
+  if pw_strength >= 50
+    message = "Congratulations on your strong password"
+  elsif (11..49) === pw_strength
+    message =  out_pw << fix_password(num_char_types, out_length)
+  elsif pw_strength <= 10
+    message = "Your password is not adequate.  Please try again."
+  end
+  message
 end
 
 def fix_password(num_char_types, num_length)
-    num_to_replace = ((50/num_char_types) + 1) - num_length
-    password_add = ""
-    num_to_replace.times do
-      password_add << @rand_char.call
-    end
-    password_add
+  num_to_replace = ((50/num_char_types) + 1) - num_length
+  return rand_char(num_to_replace)
 end
 
 def count_character_types(in_string)
@@ -39,8 +35,8 @@ def count_character_types(in_string)
     char_types += 1
   end
   if in_string =~ /[^[:ascii:]][[:alnum:]]/ ||
-     in_string =~ /[[:punct:]]/ ||
-     in_string =~ /[_*!@#$%&+=|{}]/
+      in_string =~ /[[:punct:]]/ ||
+      in_string =~ /[_*!@#$%&+=|{}]/
     char_types += 1
   end
 
@@ -53,9 +49,9 @@ def check_dictionary(in_string)
   new_string = ""
   dictionary = ['password','pass','word','per','goat','cat']
   dictionary.each do |d|
-     if (in_string.include? d)
-       return in_string.sub(d,@rand_char.call)
-     end
+    if (in_string.include? d)
+      return in_string.sub(d,rand_char(1))
+    end
   end
 end
 
